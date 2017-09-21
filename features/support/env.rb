fbc9625e8b801c1d21fa9e7a93482adb0d393ff1
wanderley Landim <wanderleylandim@wanderley-mito.corp.concrete.com.br>
2017-09-21 17:08:43 -0300

    commit Initial commit

---
+++ features/support/env.rb
@@ -0,0 +1,24 @@
+require 'rspec/expectations'
+require 'appium_lib'
+require 'cucumber/ast'
+require 'pry'
+
+# Create a custom World class so we don't pollute `Object` with Appium methods
+class AppiumWorld
+end
+
+if ENV['PLATFORM'] == 'ios'
+  caps = Appium.load_appium_txt file: File.expand_path('./../../../iosappium.txt', __FILE__), verbose: true
+elsif ENV['PLATFORM'] == 'android'
+  caps = Appium.load_appium_txt file: File.expand_path('./../../../AndroidAppium.txt', __FILE__), verbose: true
+end
+
+Appium::Driver.new(caps)
+Appium.promote_appium_methods AppiumWorld
+
+World do
+  AppiumWorld.new
+end
+
+Before { $driver.start_driver }
+After { $driver.driver_quit }
